8bcaa375fe6112b3c36327acbe6a8b49f158085a
pixIe <67672793+PixlePixle@users.noreply.github.com>
2021-12-04 17:23:42 -0500

    code update
    It's getting there :D

--- pong.py
+++ pong.py
@@ -22,49 +22,72 @@ background = BLACK
 leftpaddle = Rect(10, height/3, 15, height/5)
 rightpaddle = Rect(width-25, height/3, 15, height/5)
 ball = Rect(width/2, height/2, 20, 20)
-ballMovement = (random.randrange(1), random.randrange(1))
 
-dirx = {K_LEFT:-1, K_RIGHT:1}
-diry = {K_UP:-1, K_DOWN:1}
-v = [0,0]
+rightpaddlekeys = {K_UP:-3, K_DOWN:3}
+leftpaddlekeys = {K_w:-3, K_s:3}
+ballmovement = [random.choice([-2, 2]), random.choice([-2, 2])]
+leftpaddlemovement = 0
+rightpaddlemovement = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    
+
         if event.type == KEYDOWN:
-            if event.key in dirx:
-                v[0] +=dirx[event.key]
-            if event.key in diry:
-                v[1] +=diry[event.key]
+            if event.key in rightpaddlekeys:
+                rightpaddlemovement = rightpaddlekeys[event.key]
+            if event.key in leftpaddlekeys:
+                leftpaddlemovement = leftpaddlekeys[event.key]
+        if event.type == KEYUP:
+            if event.key in rightpaddlekeys:
+                rightpaddlemovement = 0
+            if event.key in leftpaddlekeys:
+                leftpaddlemovement = 0
                 
-    ball.x += v[0]
-    ball.y += v[1]
-    
+    if rightpaddle.top < 0:
+        rightpaddlemovement = 0
+        rightpaddle.y = 0
+    if rightpaddle.bottom > height:
+        rightpaddlemovement = 0
+        rightpaddle.y = height - height/5
+
+    if leftpaddle.top < 0:
+        leftpaddlemovement = 0
+        leftpaddle.y = 0
+    if leftpaddle.bottom > height:
+        leftpaddlemovement = 0
+        leftpaddle.y = height - height/5
+
     if ball.bottom > height:
-        v[1] = -v[1]
+        ballmovement[1] = -ballmovement[1]
     if ball.top < 0:
-        v[1] = -v[1]
+        ballmovement[1] = -ballmovement[1]
     if ball.left < 0:
         ball.x = width/2
         ball.y = height/2
-        v[0] = 0
-        v[1] = 0
+        ballmovement[0] = 0
+        ballmovement[1] = 0
         wait(300)
-        v = [random.choice([-1, 1]), random.choice([-1, 1])]
+        ballmovement = [random.choice([-2, 2]), random.choice([-2, 2])]
     if ball.right > width:
         ball.x = width/2
         ball.y = height/2
-        v[0] = 0
-        v[1] = 0
+        ballmovement[0] = 0
+        ballmovement[1] = 0
         wait(300)
-        v = [random.choice([-1, 1]), random.choice([-1, 1])]
+        ballmovement = [random.choice([-2, 2]), random.choice([-2, 2])]
     if ball.left < leftpaddle.right and ball.bottom > leftpaddle.top and ball.top < leftpaddle.bottom:
-        v[0] = -v[0]
+        ballmovement[0] = -ballmovement[0]
     if ball.right > rightpaddle.left and ball.bottom > rightpaddle.top and ball.top < rightpaddle.bottom:
-        v[0] = -v[0]
+        ballmovement[0] = -ballmovement[0]
     
+    ball.x += ballmovement[0]
+    ball.y += ballmovement[1]
+    rightpaddle.y += rightpaddlemovement
+    leftpaddle.y += leftpaddlemovement
+
     clock.tick(60)
     screen.fill (background)
+    pygame.draw.rect(screen, (100, 100, 100), (width/2, 0, 10, height))
     pygame.draw.ellipse(screen, GREEN, ball)
     pygame.draw.rect(screen,WHITE, leftpaddle)
     pygame.draw.rect(screen,WHITE, rightpaddle)
